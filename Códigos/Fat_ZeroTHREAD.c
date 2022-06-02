6312522554975465c3b6f6bec3906984832a3ee6
Kelvin Araújo Ferreira <dilli.kelvin@gmail.com>
2022-06-02 01:17:16 -0400

    Update Fat_ZeroTHREAD.c

--- Códigos/Fat_ZeroTHREAD.c
+++ Códigos/Fat_ZeroTHREAD.c
@@ -3,53 +3,53 @@
 
 #define maxSize 100000000
 
-// Assinaturas das funções
-// multiplica cada numero de fatorial pelo produto algarismo por algarismo
+// Aqui as assinaturas das funções.
+// Aqui multiplica cada numero de fatorial pelo produto algarismo por algarismo.
 int multiply(int fatNum, int buffer[], int size);
-//calcula o fatorial de um numero passado por parametro
+//Aqui calcula o fatorial de um numero passado por parametro.
 int factorial(int number);
 
-// implementações
+// Aqui as implementações.
 
 int factorial(int number){
-    int *buffer = (int*)malloc(maxSize*sizeof(int));        // define o tamanho do buffer
-    int size = 1;                                           // inicializa o tamanho do buffer
-    buffer[0] = 1;                                          // inicializa o valor do fatorial com 1
+    int *buffer = (int*)malloc(maxSize*sizeof(int));        // Aqui define o tamanho do buffer.
+    int size = 1;                                           // Aqui inicializa o tamanho do buffer.
+    buffer[0] = 1;                                          // Aqui inicializa o valor do fatorial com 1.
 
-    int i;      //define um contador
+    int i;                                                 //Aqui define um contador.
 
-    // multiplica os valores de 2 ate number e retorna o novo tamanho do buffer
+    // Aqui multiplica os valores de 2 ate number e retorna o novo tamanho do buffer.
     for(i = 2; i <= number; i++){
         size = multiply(i, buffer, size);
     }
 
-    // exibe o valor final do fatorial na tela
+    // Aqui ele exibe o valor final do fatorial na tela.
     for(i = size - 1; i >= 0; i--){
         printf("%d", buffer[i]);
     }
-    printf("\n\n");     //pula linha
-    free(buffer);       //libera a memoria do buffer
+    printf("\n\n");     //Aqui pula linha.
+    free(buffer);       //Aqui libera a memória do buffer.
     return size;
 }
 
 int multiply(int fatNum, int buffer[], int size){
-    int carry = 0;      // numero que e carregado para outras casas do numero final
+    int carry = 0;      // Aqui o número que e carregado para outras casas do numero final.
 
-    // multiplica o numero passado, algorismo por algorismo
+    // Aqui multiplica o número passado, algorismo por algorismo.
     for(int i = 0; i < size; i++){
-        // multiplica o algarismo do buffer[i] pelo valor passado e soma com o carry
+        // Aqui multiplica o algarismo do buffer[i] pelo valor passado e soma com o carry.
         int prod = buffer[i]*fatNum + carry;
 
-        // troca o valor do buffer[i] pelo resto da divisáo do produto anterior por 10
+        // Aqui vai trocar o valor do buffer[i] pelo resto da divisáo do produto anterior por 10.
         buffer[i] = prod%10;
 
-        // define o valor do carry como o quociente a divisão inteira de prod por 10
+        // Aqui define o valor do carry como o quociente a divisão inteira de prod por 10.
         carry = prod/10;
     }
     
     while(carry){
         buffer[size] = carry%10;
-        carry /= 10;                // carry = carry/10
+        carry /= 10;               
         size++;
     }
     return size;
